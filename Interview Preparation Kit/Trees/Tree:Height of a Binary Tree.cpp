1014fd6048af03211f8474ab2a1d8d61e4705298
Annie Shen <annieshenca@gmail.com>
2019-03-27 11:59:19 -0700

    Tree practice

---
+++ Interview Preparation Kit/Trees/Tree:Height of a Binary Tree.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        
+        // Recurse through the left and right branch of the root and add 1 height each time.
+        // Only return the max of either left or right.
+        // Once a node reaches the leaf and no further to go, return -1 to counter out the
+        // plus one from one recursion before.
+        if (root == NULL) {
+            return -1;
+        }
+        return max(1 + height(root->left), 1 + height(root->right));
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
+
